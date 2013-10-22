e10af15af48804ee77424028d09adbd4696c8dbb
devilmaycry4 <23732639@163.com>
2013-10-22 15:12:39 +0800

    Initial Commit

---
+++ ShareKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShareKit
+//
+//  Created by user1 on 13-10-22.
+//  Copyright (c) 2013年 user1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShareKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShareKit
+//
+//  Created by user1 on 13-10-22.
+//  Copyright (c) 2013年 user1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
